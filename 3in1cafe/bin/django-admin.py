7b6ca39d47d68f78c83e14cdac7160c473e53519
JMGC Media <jmgc@JMGCs-MacBook-Pro.local>
2019-08-07 14:44:22 -0400

    Installl all the dependencies

---
+++ 3in1cafe/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jmgc/Documents/workspace/cafeBackEnd/3in1cafe/3in1cafe/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
